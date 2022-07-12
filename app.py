24e48ae11e019705195f5c31f4d023db940c9562
qtuong1729 <62126447+qtuong1729@users.noreply.github.com>
2022-07-12 20:50:47 +0700

    up load model

--- app.py
+++ app.py
@@ -92,8 +92,7 @@ if __name__ == '__main__':
     st.write("data ready")
     st_df = st.dataframe(data.toPandas())
     ## Load model
-    model_lr.load('./model/linear_regression/lr_basic')
-    model_full = LinearRegressionModel.load(sc,"./model/lr_basic")
+    model_lr = LinearRegressionModel.load("./model/lr_basic")
     st.write("have lr")
 
 
